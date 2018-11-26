82cc2989787cc2f1f026666b4f7bde7a41ad0512
rudlevsky <rudlevsky@gmail.com>
2018-11-26 11:10:00 +0300

    sixth task

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,16 +1,33 @@
 ﻿using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace No6.Solution.Tests
 {
     [TestFixture]
     public class CustomEnumerableTests
     {
+        private static int FirstSequance(int second, int first) 
+        =>  second + first;
+
+        private static int SecondSequance(int second, int first)
+        => 6 * second - 8 * first;
+
+        private static double ThirdSequance(double second, double first)
+        => second +  first / second;  
+
         [Test]
         public void Generator_ForSequence1()
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            var list = new List<int>();
+
+            foreach (var item in Generator<int>.Generate(1, 1, 10, FirstSequance))
+            {
+                list.Add(item);
+            }
+
+            CollectionAssert.AreEqual(expected, list);
         }
 
         [Test]
@@ -18,7 +35,14 @@ namespace No6.Solution.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            var list = new List<int>();
+
+            foreach (var item in Generator<int>.Generate(1, 2, 10, SecondSequance))
+            {
+                list.Add(item);
+            }
+
+            CollectionAssert.AreEqual(expected, list);
         }
 
         [Test]
@@ -26,7 +50,17 @@ namespace No6.Solution.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            var list = new List<double>();
+
+            foreach (var item in Generator<double>.Generate(1, 2, 10, ThirdSequance))
+            {
+                list.Add(item);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Assert.AreEqual(expected[i], list[i], 0.000001);
+            }
         }
     }
 }
---
+++ No6/Generator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No6
+{
+    public static class Generator<T>
+    {
+        public static IEnumerable<T> Generate(T first, T second, int n, Func<T, T, T> func)
+        {
+            yield return first;
+            yield return second;
+
+            for (int i = 2; i < n; i++)
+            {
+                T temp = func(second, first);
+
+                yield return temp;
+
+                first = second;
+                second = temp;
+            }
+        }
+    }
+}
